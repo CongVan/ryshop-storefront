7b74323b0a4ccac903b7bc814864fd4b6d29a1cd
VanHC <vanhc@galaxy.com.vn>
2022-08-08 19:41:09 +0700

    init source

---
+++ src/modules/checkout/components/payment-cash-on-devlivery/index.tsx
@@ -0,0 +1,14 @@
+const PaymentCashOnDelivery = () => {
+  return (
+    <div className="w-full">
+      <div className="flex items-center gap-x-2 w-full p-2">
+        {/* <Alert size={16} className="text-yellow-700" /> */}
+        <span className="text-small-regular">
+          Thanh toán khi nhận hàng, trả hàng miễn phí sau 7 ngày (*)
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default PaymentCashOnDelivery
